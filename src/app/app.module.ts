e696343262b5f36fd19cf74c410f7792383f9f19
Mykola Samila <samila.mykola@gmail.com>
2022-08-10 13:43:51 +0200

    Added idea model. Added basic idea data on front end

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,19 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { GetIdeasComponent } from './get-ideas/get-ideas.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import {MatButtonModule} from '@angular/material/button';
+import { MatButtonModule } from '@angular/material/button';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    GetIdeasComponent
-  ],
-  imports: [
-    BrowserModule,
-    BrowserAnimationsModule,
-    MatButtonModule
-  ],
+  declarations: [AppComponent, GetIdeasComponent],
+  imports: [BrowserModule, BrowserAnimationsModule, MatButtonModule, HttpClientModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
