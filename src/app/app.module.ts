e89e43ddc9d1fe9bbc0af1ab88d2b6f7282e6453
Mykola Samila <samila.mykola@gmail.com>
2022-08-10 14:34:35 +0200

    Added participants counter

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,11 +5,18 @@ import { AppComponent } from './app.component';
 import { GetIdeasComponent } from './get-ideas/get-ideas.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
 import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [AppComponent, GetIdeasComponent],
-  imports: [BrowserModule, BrowserAnimationsModule, MatButtonModule, HttpClientModule],
+  imports: [
+    BrowserModule,
+    BrowserAnimationsModule,
+    MatButtonModule,
+    HttpClientModule,
+    MatIconModule,
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
