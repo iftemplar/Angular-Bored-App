483e1b052271c90b2032cee57d5d776bb372acbe
Mykola Samila <samila.mykola@gmail.com>
2022-08-10 18:54:22 +0200

    Added Header

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,16 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
-import { AppComponent } from './app.component';
-import { GetIdeasComponent } from './get-ideas/get-ideas.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
 import { HttpClientModule } from '@angular/common/http';
 
+import { AppComponent } from './app.component';
+import { GetIdeasComponent } from './get-ideas/get-ideas.component';
+import { HeaderComponent } from './header/header.component';
+
 @NgModule({
-  declarations: [AppComponent, GetIdeasComponent],
+  declarations: [AppComponent, GetIdeasComponent, HeaderComponent],
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
